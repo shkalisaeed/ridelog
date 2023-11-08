3744c030430555c6283383cecd70ed76cec75fd5
shkalisaeed <128216564+shkalisaeed@users.noreply.github.com>
2023-11-08 12:11:14 +1100

    Updated index.jsx
    added login option with email and password

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,11 +1,98 @@
 import { auth, provider } from "../../config/firebase-config";
-import { signInWithPopup } from "firebase/auth";
+import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
+import {createUserWithEmailAndPassword, onAuthStateChanged} from "firebase/auth"
 import "./styles.css";
 import logo from "./logo_r.png"; // Import the logo image
+import { updateProfile } from "firebase/auth";
+
+import { useState, useEffect } from "react";
 
 export const Auth = () => {
     const navigate = useNavigate();
+
+    const [registerEmail, setRegisterEmail] = useState("");
+    const [registerName, setRegisterName] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+    const [user, setUser] = useState({});
+
+
+    const register = async () => {
+        if (validateEmail(registerEmail) && validatePassword(registerPassword)) {
+            try {
+                const userdata = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);     
+               // Set the user's name in the Firebase user object
+                await updateProfile(userdata.user, {
+                    displayName: registerName,
+                });
+
+            const authInfo2 = {
+                userID: userdata.user.uid,
+                name: registerName, // Using the captured name
+                profilePhoto: userdata.user.photoURL,
+                isAuth: true,
+            };
+
+
+            localStorage.setItem("auth", JSON.stringify(authInfo2)); //storing info locally in object
+            navigate("/expense-tracker");
+                
+              //  navigate("/expense-tracker");
+            
+                console.log(userdata);
+            }
+            
+             catch (error) {
+                console.log(error.message);
+            }
+        } else {
+            // Provide feedback to the user about email and password requirements.
+            alert("Please enter a valid email address and a password with a minimum of 6 characters.");
+        }
+    };
+
+    const login = async () => {
+        if (validateEmail(loginEmail) && validatePassword(loginPassword)) {
+            try {
+                const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+
+
+                const authInfo3 = {
+                    userID: user.user.uid,
+                    name: user.displayName,
+                    profilePhoto: user.photoURL,
+                    isAuth: true,
+                };
+    
+    
+                localStorage.setItem("auth", JSON.stringify(authInfo3)); //storing info locally in object
+                    navigate("/expense-tracker");
+                console.log(user);
+            } catch (error) {
+                console.log(error.message);
+            }
+        } else {
+            // Provide feedback to the user about email and password requirements.
+            alert("Please enter a valid email address and a password with a minimum of 6 characters.");
+        }
+    };
+
+    const validateEmail = (email) => {
+        // Use a simple regular expression to check for the presence of '@' and '.com'.
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    };
+
+    const validatePassword = (password) => {
+        return password.length >= 6;
+    };
+
+    const logout = async () => {
+        // Implement logout logic if needed.
+    };
+   
+
 
     const signInWithGoogle = async () => {
         try {
@@ -36,10 +123,72 @@ export const Auth = () => {
 
     return (
         <div className="login-page">
-            <img src={logo} alt="RIDELOG Logo" style={{ width: "50%" }} /> {/* Apply 50% width to reduce size */}
-            <h1>RIDELOG Vehicle Expense Tracker</h1>
-            <p>Sign in with a Google account</p>
-            <button onClick={signInWithGoogle}>Sign In</button>
+
+            <header>
+                <nav>
+                    <ul className="top-nav">
+                        <li><a href="#">FAQs</a></li>
+                        <li><a href="#">Our Mission</a></li>
+                        <li><a href="#">Team</a></li>
+                    </ul>
+                </nav>
+                
+            </header>
+            <img src={logo} alt="RIDELOG Logo" style={{ width: "50%" }} />
+                
+
+
+
+            <div className="login-data">
+           
+
+            <div>
+                <h3>Vehicle Expense Tracker</h3>
+                <h4>Login</h4>
+                <input placeholder="Email.."
+                    onChange={(event) => {
+                        setLoginEmail(event.target.value);
+                    }}
+                />
+                <input 
+                type="password"
+                placeholder="Password.."
+                onChange={(event) => {
+                        setLoginPassword(event.target.value);
+                    }}
+                />
+                <button onClick={login}>Sign In</button>  
+            </div>
+            <p>Or</p>
+             
+            <button onClick={signInWithGoogle}>Sign in with Google</button>
+            
+            <div>
+                <h4>Register User</h4>
+                <input placeholder="Name.."
+                    onChange={(event) => {
+                        setRegisterName(event.target.value);
+                    }}
+                />
+                
+                <input placeholder="Email.."
+                    onChange={(event) => {
+                        setRegisterEmail(event.target.value);
+                    }}
+                />
+                <input 
+                 type="password"
+                 placeholder="Password.."
+                 onChange={(event) => {
+                    setRegisterPassword(event.target.value);
+                    }}
+                />
+                <button onClick={register}>Create User</button>
+            </div>
+
+
+           
         </div>
+    </div>
     );
 };
