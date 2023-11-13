fd5ef639e97fba3b901deddf67174275df9f37df
shkalisaeed <128216564+shkalisaeed@users.noreply.github.com>
2023-11-13 15:04:16 +1100

    Update useAddTransaction.js

--- src/hooks/useAddTransaction.js
+++ src/hooks/useAddTransaction.js
@@ -1,25 +1,35 @@
-import {addDoc, collection, serverTimestamp} from "firebase/firestore";
-import{db} from "../config/firebase-config";
-import {useGetUserInfo} from "./useGetUserInfo";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../config/firebase-config";
+import { useGetUserInfo } from "./useGetUserInfo";
 
 export const useAddTransaction = () => {
-    const transactionCollectionRef = collection(db, "transactions")
-    const {userID} = useGetUserInfo();
-    const addTransaction = async ({
-        description,
-        transactionAmount,
-        transactionType,
-    }) => {
-        
-        await addDoc(transactionCollectionRef, {
+   const transactionCollectionRef = collection(db, "transactions");
+   const { userID } = useGetUserInfo();
+
+   const addTransaction = async ({
+      rego, // Add rego to the parameters
+      transactionType,
+      transactionAmount,
+      description,
+      transactionDate,
+   }) => {
+      try {
+         // You may want to validate the input data here
+
+         await addDoc(transactionCollectionRef, {
             userID,
+            rego, // Include the rego field
             description,
             transactionAmount,
             transactionType,
+            transactionDate,
             createdAt: serverTimestamp(),
+         });
+      } catch (error) {
+         console.error("Error adding transaction: ", error);
+         // Handle error as needed
+      }
+   };
 
-        });
-    };
-    
-    return{addTransaction};
+   return { addTransaction };
 };
