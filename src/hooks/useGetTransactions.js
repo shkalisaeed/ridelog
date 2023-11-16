007c0d90011fb1c0e3a0ea35d6d762e018d876ac
shkalisaeed <128216564+shkalisaeed@users.noreply.github.com>
2023-11-17 10:18:45 +1100

    Add files via upload

---
+++ src/hooks/useGetTransactions.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { collection, doc, orderBy, query, where, onSnapshot } from "firebase/firestore";
+import { db } from "../config/firebase-config";
+import { useGetUserInfo } from "./useGetUserInfo";
+
+
+export const useGetTransactions = () => {
+    const [transactions, setTransactions] = useState([]);
+
+    const transactionCollectionRef = collection(db, "transactions");
+    const {userID} = useGetUserInfo();
+
+    const getTransactions = async () => {
+       let unsubscribe;
+        try{
+             const queryTransactions = query(
+                transactionCollectionRef, 
+                where("userID","==", userID),
+                orderBy("createdAt")
+             );
+            unsubscribe = onSnapshot(queryTransactions, (snapshot) => {
+                    
+                    let docs = [];
+
+                    snapshot.forEach((doc) => {
+                        const data = doc.data();
+                        const id = doc.id;
+                        docs.push({...data, id });
+                    });
+                    setTransactions(docs);
+                });
+        } catch (err) 
+        {
+            console.error(err);
+        }
+        return() => unsubscribe();
+    };
+   
+   
+    useEffect(() => {
+       getTransactions() 
+    }, [])
+
+    return{transactions};
+};
