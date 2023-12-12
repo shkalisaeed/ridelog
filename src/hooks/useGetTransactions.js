b92b116ad0e2b03a289e42e931492dbb8414e6f4
shkalisaeed <128216564+shkalisaeed@users.noreply.github.com>
2023-12-12 12:47:56 +1100

    Update useGetTransactions.js
    changing hook to add delete function

--- src/hooks/useGetTransactions.js
+++ src/hooks/useGetTransactions.js
@@ -1,45 +1,54 @@
+// useGetTransactions.js
+
 import { useEffect, useState } from "react";
-import { collection, doc, orderBy, query, where, onSnapshot } from "firebase/firestore";
+import { collection, doc, orderBy, query, where, onSnapshot, deleteDoc } from "firebase/firestore";
 import { db } from "../config/firebase-config";
 import { useGetUserInfo } from "./useGetUserInfo";
 
-
 export const useGetTransactions = () => {
-    const [transactions, setTransactions] = useState([]);
+  const [transactions, setTransactions] = useState([]);
+  const { userID } = useGetUserInfo();
 
-    const transactionCollectionRef = collection(db, "transactions");
-    const {userID} = useGetUserInfo();
+  const transactionCollectionRef = collection(db, "transactions");
 
-    const getTransactions = async () => {
-       let unsubscribe;
-        try{
-             const queryTransactions = query(
-                transactionCollectionRef, 
-                where("userID","==", userID),
-                orderBy("createdAt")
-             );
-            unsubscribe = onSnapshot(queryTransactions, (snapshot) => {
-                    
-                    let docs = [];
+  const deleteTransaction = async (transactionId) => {
+    const transactionDocRef = doc(db, "transactions", transactionId);
 
-                    snapshot.forEach((doc) => {
-                        const data = doc.data();
-                        const id = doc.id;
-                        docs.push({...data, id });
-                    });
-                    setTransactions(docs);
-                });
-        } catch (err) 
-        {
-            console.error(err);
-        }
-        return() => unsubscribe();
-    };
-   
-   
-    useEffect(() => {
-       getTransactions() 
-    }, [])
+    try {
+      await deleteDoc(transactionDocRef);
+      console.log("Transaction deleted from Firestore:", transactionId);
+    } catch (error) {
+      console.error("Error deleting transaction from Firestore:", error.message);
+      throw error;
+    }
+  };
 
-    return{transactions};
+  const getTransactions = async () => {
+    let unsubscribe;
+    try {
+      const queryTransactions = query(
+        transactionCollectionRef,
+        where("userID", "==", userID),
+        orderBy("createdAt")
+      );
+      unsubscribe = onSnapshot(queryTransactions, (snapshot) => {
+        let docs = [];
+        snapshot.forEach((doc) => {
+          const data = doc.data();
+          const id = doc.id;
+          docs.push({ ...data, id });
+        });
+        setTransactions(docs);
+      });
+    } catch (err) {
+      console.error(err);
+    }
+    return () => unsubscribe();
+  };
+
+  useEffect(() => {
+    getTransactions();
+  }, []);
+
+  return { transactions, setTransactions, deleteTransaction };
 };
