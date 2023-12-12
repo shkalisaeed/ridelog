65deaa5aa7afca0a38b585cc6024c915c72775db
shkalisaeed <128216564+shkalisaeed@users.noreply.github.com>
2023-12-12 12:45:47 +1100

    Update index.jsx

--- src/pages/mission/index.jsx
+++ src/pages/mission/index.jsx
@@ -9,7 +9,7 @@ export const Mission = () => {
         <>
             <div className='Mission'>
                 <header>
-                    <h1>RideLog Vision and Mission Page</h1>
+                    <h1>VISION   &nbsp;&   &nbsp;MISSION   &nbsp;STATEMENT</h1>
                     <nav>
                         <button onClick={() => navigate(-1)}>Return</button>
                     </nav>
@@ -17,12 +17,12 @@ export const Mission = () => {
 
                 <section>
                     <div className="vision-mission-content">
-                        <h2>Vision Statement:</h2>
+                        <h3>Vision Statement:</h3>
                         <p>
                             To be the leading force in transforming the way people perceive and manage vehicle expenses globally. We envision a future where every individual and organization effortlessly navigates the intricate landscape of transportation costs through our intuitive and comprehensive solutions. By fostering a culture of continuous innovation, reliability, and customer-centricity, we strive to become the go-to platform that empowers users to make informed decisions, maximize savings, and achieve financial success in their vehicular journeys.
                         </p>
 
-                        <h2>Mission Statement:</h2>
+                        <h3>Mission Statement:</h3>
                         <p>
                             At RideLog, our mission is to empower individuals and businesses by revolutionizing how they manage their vehicle expenses. We are committed to providing innovative and user-friendly solutions that streamline the tracking and optimization of transportation-related costs. By harnessing cutting-edge technology, we aim to simplify the complexities of expense management, saving time and resources while fostering financial transparency and efficiency.
                         </p>
