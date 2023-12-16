36fb3daaf696e1b50780241a448b4f0f36c06a64
shkalisaeed <128216564+shkalisaeed@users.noreply.github.com>
2023-12-16 12:11:08 +1100

    Update index.jsx

--- src/pages/team/index.jsx
+++ src/pages/team/index.jsx
@@ -28,30 +28,30 @@ export const Team = () => {
                         </div>
 
                         <div className="team-member">
-                            <h3>Shuyun - Developer</h3>
+                            <h3>Sania - Scrum Master</h3>
                             <p>
-                                Shuyun brings valuable technical skills to our team, contributing to the development and enhancement of our vehicle expense tracker. With a passion for coding and problem-solving, Shuyun plays a crucial role in bringing our product to life.
+                                Sania brings valuable agile skills to our team, contributing to the development and enhancement of our vehicle expense tracker. With a passion for coding and problem-solving, Shuyun plays a crucial role in bringing our product to life.
                             </p>
                         </div>
 
                         <div className="team-member">
-                            <h3>Yaness - Developer</h3>
+                            <h3>Resham - Finance</h3>
                             <p>
-                                Yaness is a key member of our development team, bringing expertise in Cloud Deployment. With a focus on innovation, Yaness contributes to the continuous improvement of our vehicle expense tracking solution.
+                                Resham is a key member and finance manager at our team. With a focus on innovation, Resham contributes to the continuous improvement of finance and expenses related our vehicle expense tracking solution.
                             </p>
                         </div>
 
                         <div className="team-member">
-                            <h3>Oscar - Developer</h3>
+                            <h3>Simran - Marketing Head</h3>
                             <p>
-                            Oscar is a skilled developer who plays a vital role in shaping the functionality and features of our vehicle expense tracker. With a keen eye for detail and a commitment to quality, Oscar ensures that our product meets the highest standards.
+                            Simran is a skilled marketing person. With a keen eye for detail and a commitment to quality, Simran ensures that our product meets the highest standards.
                             </p>
                         </div>
                     </div>
                     <div className="team-member center-bottom">
-                            <h3>Renard - Developer</h3>
+                            <h3>Srujan - Developer </h3>
                             <p>
-                                Renard is an integral part of our development team, bringing enthusiasm and expertise to the table. With a background in HTML and CSS, Renard contributes to the success of our vehicle expense tracker through innovative coding and problem-solving.
+                                Srujan is an integral part of our development team, bringing enthusiasm and expertise to the table. With a background in HTML and CSS, Srujan contributes to the success of our vehicle expense tracker through innovative coding and problem-solving.
                             </p>
                         </div>
                 </div>
